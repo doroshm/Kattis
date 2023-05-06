de27f401a89acaca036512597f3aeb56b9a9b96c
doroshm <115737819+doroshm@users.noreply.github.com>
2023-05-06 16:06:26 -0400

    Add files via upload

---
+++ transitwoes.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class transitwoes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt(), t = in.nextInt(), b = in.nextInt();
+        
+        int[] arr = new int[b * 2 + 1];
+        for (int i = 0; i < arr.length; i += 2) {
+            arr[i] = in.nextInt();
+        }
+        
+        for (int i = 1; i < arr.length; i += 2) {
+            arr[i] = in.nextInt();
+        }
+
+        String out = "yes";
+        s += arr[0];
+        for (int i = 1; i < arr.length; i += 2) {
+            int bTime = in.nextInt();
+            int bPos = 0;
+            while (bPos < s) {
+                bPos += bTime;
+            }
+
+            s += (bPos - s) + arr[i] + arr[i + 1];
+            
+            if (s > t) {
+                out = "no";
+                break;
+            }
+        }
+        in.close();
+        System.out.println(out);
+    }
+}
