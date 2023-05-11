b529ebeeb6ee029e8bbe2d46dfc410f2c72b7c22
doroshm <115737819+doroshm@users.noreply.github.com>
2023-05-10 21:46:13 -0400

    Update grading.java

--- grading.java
+++ grading.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class grading {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] limits = new int[5];
-        char[] grades = {'A', 'B', 'C', 'D', 'E'};
+        int[] limits = new int[6];
+        char[] grades = {'A', 'B', 'C', 'D', 'E', 'F'};
         for (int i = 0; i < 5; i++) {
             limits[i] = in.nextInt();
         }
         int exam = in.nextInt();
         in.close();
-        char out = 'F';
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i <= 5; i++) {
             if (exam >= limits[i]) {
-                out = grades[i];
+                System.out.println(grades[i]);
                 break;
             }
         }
-        System.out.println(out);
     }
 }
