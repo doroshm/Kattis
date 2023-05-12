6d26474026e8a573381910a98061b9c0c973d00d
doroshm <115737819+doroshm@users.noreply.github.com>
2023-05-12 16:58:23 -0400

    Add files via upload

---
+++ estimatingtheareaofacircle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class estimatingtheareaofacircle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double r = in.nextDouble(), m = in.nextDouble(), c = in.nextDouble();
+        while (r != 0) {
+            System.out.println(Math.PI * Math.pow(r, 2) + " " + c/m * Math.pow(r*2, 2));
+            r = in.nextDouble();
+            m = in.nextDouble();
+            c = in.nextDouble();
+        }
+        in.close();
+    }
+}
